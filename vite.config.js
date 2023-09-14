533eb0c116aa35cd0ec5ffca83b65f87f06df8af
Vanessa Proaño <vaneproano91@gmail.com>
2023-09-14 14:14:41 -0500

    Corregir configuración de la base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://mishka911.gitbuh.io/Creador-Tareas-REACT-VITE",
+  base: "/Creador-Tareas-REACT-VITE/",
 });
